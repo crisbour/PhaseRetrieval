b2e76b613513f1cbf7ad1d63da0c569288cf4e5a
Cristian Bourceanu <bourceanu.cristi@gmail.com>
2020-05-15 23:15:08 +0100

    gitignore4

--- CUDA Implementation/Debug/deprecated_eclipse/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_core -lcufft -lopencv_imgproc -lopencv_imgcodecs -lopencv_highgui
-
